46cd71a38b77eb93b259f692557286237ccab3f3
RickardOtv <98350189+RickardOtv@users.noreply.github.com>
2022-12-16 16:06:49 +0100

    Update mrny.js

--- mrny.js
+++ mrny.js
@@ -1,1 +1,4 @@
 //mah name jeff
+menu = new Menu("Mr. Ny", 0, 0, 100, 100); // Create a new menu
+menu.addMenuItem("Mr. Ny", "Mr. Ny"); // Add a menu item
+menu.addMenuItem("Mr. Ny", "Mr. Ny"); // Add a menu item
